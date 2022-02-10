ca4ff9f644d544d2337b3efbcb89097b93232237
asaju67 <ashley.saju67@gmail.com>
2022-02-10 06:15:58 -0600

    done: code for going 0 to the limit

--- src/HighestPrime.java
+++ src/HighestPrime.java
@@ -4,26 +4,28 @@ public class HighestPrime {
     public static void main(String[] args) {
 
 
-        System.out.println(findHighestPrime(10));
+        System.out.println(findHighestPrime(10000000));
     }
 
     public static int findHighestPrime(int limit) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
         if (limit > 1) {
             primes.add(2);
-
-            for (int x = 3; x <= limit; x++) {
-                for (int y = 0; y < primes.size(); y++) {
-                    do
-                    if (x % y != 0) {
+            for (int x = 3; x < limit + 1; x++) {
+                int y = 0;
+                do {
+                    if ((x % primes.get(y) != 0) && (y == primes.size() - 1)) {
                         primes.add(x);
                     }
-                    while(x % y == 0);
-                }
+                    y++;
+                }while(x % primes.get(y) != 0);
+
 
             }
-        } else {
+        } else if (limit == 1) {
             primes.add(1);
+        } else if (limit <= 0) {
+            primes.add(0);
         }
         return primes.get(primes.size() - 1);
 
